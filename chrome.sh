c76472b1b290fe98f33f7cb0f6bd7e58452af8d6
Reza Kargar <r.kargar.2014@gmail.com>
2025-01-29 19:32:25 +0330

    Add script to open Arc browser

---
+++ chrome.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Chrome
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🌐
+# @raycast.packageName Util
+
+# Documentation:
+# @raycast.description Opens the Arc when instead of Chrome
+# @raycast.author Reza Kargar
+# @raycast.authorURL https://rezakargar.ir
+
+open -a "Arc"
+
+echo "Arc opened"
